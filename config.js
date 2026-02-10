9445783d03354b9855eba50b2699c0da7ddab8f8
Uwais Jawed <142025750+uwais-jawed@users.noreply.github.com>
2026-02-10 13:50:21 +0530

    Update celebration message to enhance user engagement with a playful phrase

--- config.js
+++ config.js
@@ -73,7 +73,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get your gift, a machine wali chummmmiiii!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
